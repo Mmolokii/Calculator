37d7794e05b9691a23032f905c20759ec8776a02
Mmolokii <90108472+Mmolokii@users.noreply.github.com>
2024-12-08 11:09:06 +0200

    Adding the operate function logic

--- script.js
+++ script.js
@@ -11,5 +11,23 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(b === 0){
+        return "Error: Division by zero!";
+    }
     return a / b; 
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return add(num1, num2); 
+        case '-': 
+            return subtract(num1, num2); 
+        case '*': 
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2); 
+        default: 
+            return "Error: Invalid operator!"; 
+    }
 }
